a0d1c24a9368124cbfdd2b21b30286be4311ab32
jjventeo-sudo <jjventeo@gmail.com>
2026-09-25 22:56:37 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
 
    VERSION cambia con cada construcción, así que una app nueva se instala sola
    y la anterior se borra. */
-const VERSION = 'colores-2e0399e3';
+const VERSION = 'colores-d5c134c3';
 const FICHEROS = ['./', './index.html', './manifest.webmanifest',
                   './icono-192.png', './icono-512.png'];
 
